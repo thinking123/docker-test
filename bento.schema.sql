b1b78ad91595792c612fbd30df2f194b50f56165
fising <fising@qq.com>
2017-12-15 22:28:06 +0800

    files

---
+++ bento.schema.sql
@@ -0,0 +1,79 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : bento
+ Source Server Type    : MySQL
+ Source Server Version : 50720
+ Source Host           : 127.0.0.1
+ Source Database       : bento_dev
+
+ Target Server Type    : MySQL
+ Target Server Version : 50720
+ File Encoding         : utf-8
+
+ Date: 12/15/2017 22:26:17 PM
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `File`
+-- ----------------------------
+DROP TABLE IF EXISTS `File`;
+CREATE TABLE `File` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '文件ID',
+  `name` varchar(100) NOT NULL DEFAULT '' COMMENT '文件名',
+  `userId` int(11) unsigned DEFAULT NULL COMMENT '用户ID',
+  `teamId` int(11) unsigned DEFAULT NULL COMMENT '用户组ID',
+  `access` enum('0','1') NOT NULL DEFAULT '0' COMMENT '文件访问权限(0:私有1:公共)',
+  `status` enum('0','1') NOT NULL DEFAULT '1' COMMENT '文件状态(0:已删除1:正常)',
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updatedAt` timestamp NULL DEFAULT NULL COMMENT '最后修改时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_userId` (`userId`),
+  KEY `idx_teamId` (`teamId`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `Token`
+-- ----------------------------
+DROP TABLE IF EXISTS `Token`;
+CREATE TABLE `Token` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `userId` int(11) unsigned NOT NULL COMMENT '用户ID',
+  `agent` varchar(250) DEFAULT NULL COMMENT '客户端 Agent',
+  `ip` varchar(40) NOT NULL COMMENT 'IP 地址',
+  `accessToken` varchar(40) DEFAULT NULL COMMENT 'access token',
+  `refreshToken` varchar(40) NOT NULL COMMENT 'refresh token',
+  `accessTokenExpiredAt` timestamp NULL DEFAULT NULL,
+  `refreshTokenExpiredAt` timestamp NULL DEFAULT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updatedAt` timestamp NULL DEFAULT NULL COMMENT '更新时间',
+  `status` enum('0','1') NOT NULL DEFAULT '1' COMMENT 'Token 状态(0:已删除, 1:正常)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_refreshToken` (`refreshToken`) USING HASH,
+  UNIQUE KEY `uniq_accessToken` (`accessToken`) USING HASH,
+  KEY `idx_userId` (`userId`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `User`
+-- ----------------------------
+DROP TABLE IF EXISTS `User`;
+CREATE TABLE `User` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `name` varchar(120) DEFAULT NULL COMMENT '用户名称',
+  `givenName` varchar(60) DEFAULT NULL COMMENT '用户的名',
+  `familyName` varchar(60) DEFAULT NULL COMMENT '用户的姓',
+  `avatar` varchar(255) DEFAULT NULL COMMENT '头像地址',
+  `email` varchar(255) DEFAULT NULL COMMENT '电子邮件地址',
+  `googleId` varchar(21) DEFAULT NULL COMMENT 'Google ID',
+  `salt` varchar(40) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_googleId` (`googleId`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
