080b05aaac9431436fdd0d42484bd13110562bc8
fising <fising@qq.com>
2018-03-27 16:27:43 +0800

    run redis server

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM registry-vpc.cn-shanghai.aliyuncs.com/bento/api:0.0.4
 
 MAINTAINER fising <fising@qq.com>
 
+RUN sed 's/daemonize yes/# daemonize yes/' /usr/local/redis/etc/redis.conf > /etc/redis.conf
+
 EXPOSE 80 3306 6379
 
 VOLUME ["/data/apps/api"]
--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ docker run -dit --name api -p 80:80 -p 3306:3306 -p 6379:6379 -v `pwd`:/data/app
 docker exec -it api mkdir -p /data/apps/api/storage
 docker exec -it api chmod +777 -R /data/apps/api/storage
 docker exec -it api lnmp start
-docker exec -it api /usr/local/redis/bin/redis-server /usr/local/redis/etc/redis.conf
+docker exec -dit api /usr/local/redis/bin/redis-server /etc/redis.conf
 docker exec -it api mysql -uroot -p123456 -e "create database bento_dev"
 docker exec -it api mysql -uroot -p123456 -e "use bento_dev; source /data/apps/api/bento.schema.sql;"
